b6730a8e49322c7e3e518d27a12994ce7b05f324
Riccardo <120653812+riccardomortier@users.noreply.github.com>
2024-04-19 14:21:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const form = document.querySelector('form');
-const fullName = document.getElementById('form_name');
-const email = document.getElementById('form_email');
-const phone = document.getElementById('form_phoneNumber');
-const subject = document.getElementById('form_subject');
-const mess = document.getElementById('form_message');
-
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
@@ -30,27 +23,3 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
-
-function sendEmail() {
-    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value ? phone.value : '?'}<br> Message: ${mess.value}`
-
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "riccardo.mortier1@gmail.com",
-        Password : "C26FE9B0FD3622137218127607331960DE6D",
-        To : 'riccardo.mortier1@gmail.com',
-        From: 'riccardo.mortier1@gmail.com',
-        Subject : subject.value,
-        Body : bodyMessage
-    }).then(
-      message => alert(message)
-    );
-}
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    alert('The contact form is still being developed to properly work!')
-
-    // sendEmail();
-});
