9a76ac5a2dedd54823a2ed24220432cc10fb4848
Riccardo <120653812+riccardomortier@users.noreply.github.com>
2024-04-19 19:47:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('header navbar a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -13,7 +13,7 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+                document.querySelector('header navbar a [href*=' + id + ']').classList.add('active');
             });
         }
     });
